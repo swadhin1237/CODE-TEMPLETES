058af6468514f94e7034478ba8bc09e90d430c8a
SWADHIN SATYAPRAKASH MAJHI <95016250+swadhin1237@users.noreply.github.com>
2023-05-03 15:57:49 +0530

    Making changes in the segment tree

--- segmentTree.cpp
+++ segmentTree.cpp
@@ -1,21 +1,64 @@
 #include<bits/stdc++.h>
+
 using namespace std;
-const int SIZE = 100005;
-int st[4 * SIZE];
-int arr[SIZE];
 
-void build(int l,int r,int pos)
+template <typename T>
+class segmentTree
 {
-    if(l==r)
+public:
+    vector<T> st;
+    vector<T> arr;
+    segmentTree(int n)
     {
-        st[pos] = arr[l];
+        st = vector<T>(4 * n);
+        arr = vector<T>(n);
     }
-    build(l, mid, 2 * pos + 1);
-    build(mid + 1, r, 2 * pos + 2);
+    //BUILD TREE
+    void build(int l, int r, int pos)
+    {
+        if (l == r)
+        {
+            st[pos] = arr[l];
+        }
+        int mid = (l + r) / 2;
+        build(l, mid, 2 * pos + 1);
+        build(mid + 1, r, 2 * pos + 2);
+        // LOGIC
+    }
+    //POINT UPDATE
+    void update(int l,int r,int pos,int idx,T x)
+    {
+        if(r<idx||l>idx)
+        {
+            return;
+        }
+        if(l==r&&l==idx)
+        {
+            st[pos] = arr[l] = x;
+        }
+        int mid = (l + r) / 2;
+        update(l, mid, 2 * pos + 1,idx,x);
+        update(mid + 1, r, 2 * pos + 2, idx, x);
+        // LOGIC
+    }
+    //FIND ANSWER FOR QUERY
+    T search(int l,int r,int pos,int ql,int qr)
+    {
+        if(l>qr||r<ql)
+        {
+            //RETURN THE RELEVENT STUFF
+            return 0;
+        }
+        if(l>=ql&&r<=qr)
+        {
+            return st[pos];
+        }
+        int mid = (l + r) / 2;
+        //LOGIC TO SEARCH 
+    }
+};
 
-    //LOGIC
-    
-}
+
 void solve()
 {
     int n;
